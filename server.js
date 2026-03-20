b389dd849e6a2570e253024aca29533e8e96fd90
Nitishkumar63 <nitishkumar22553@gmail.com>
2026-03-20 22:55:40 +0530

    Backend initial commit

--- server.js
+++ server.js
@@ -7,20 +7,8 @@ const app = express();
 
 connectDB();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://ai-project-eight-flax.vercel.app"
-];
-
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true); // allow server-to-server requests
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin: "*",
 }));
 app.use(express.json());
 
