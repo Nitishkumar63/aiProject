029329c06473bd034673644a7b602728e498266f
Nitishkumar63 <nitishkumar22553@gmail.com>
2026-03-20 23:05:18 +0530

    changes

--- src/api.jsx
+++ src/api.jsx
@@ -5,7 +5,6 @@ const API = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, 
 });
 
 export const askAI = (prompt) => API.post("/ask-ai", { prompt });
