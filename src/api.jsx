857db95a8f599f873acc78288c19e13605fdaf71
Nitishkumar63 <nitishkumar22553@gmail.com>
2026-03-20 23:02:47 +0530

    changes

--- src/api.jsx
+++ src/api.jsx
@@ -2,8 +2,11 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://aiproject-8jxx.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true, 
 });
 
 export const askAI = (prompt) => API.post("/ask-ai", { prompt });
-
 export const saveChat = (data) => API.post("/save", data);
