33ee098d11638fb296ca1bfd01a985ac97c92987
Nitishkumar63 <nitishkumar22553@gmail.com>
2026-03-20 22:25:34 +0530

    Backend initial commit

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://aiproject-8jxx.onrender.com/api",
 });
 
 export const askAI = (prompt) => API.post("/ask-ai", { prompt });
